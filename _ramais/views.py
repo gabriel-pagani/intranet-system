586ff37cd9cd01e14245f38296a59b6dc9d624c5
Gabriel Souza <gabriel.souza@sinasc.com.br>
2025-06-16 16:01:17 -0300

    Removi a obrigatoriedade do login para a visão dos ramais

--- _ramais/views.py
+++ _ramais/views.py
@@ -1,17 +1,14 @@
 from _ramais.models import Ramais
 from django.http import JsonResponse
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 
 
-@login_required
 def home_view(request):
     return render(request, 'ramais/main.html', {
         'is_staff': request.user.is_staff
     })
 
 
-@login_required
 def ramais_json(request):
     dados = list(Ramais.objects.values('nome', 'ramal', 'setor', 'maquina'))
     return JsonResponse(dados, safe=False)
