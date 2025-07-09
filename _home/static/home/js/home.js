cc3bdde8175d1099591c07b6ba603fcd92d5b3c0
Gabriel Souza <gabriel.souza@sinasc.com.br>
2025-07-09 09:02:00 -0300

    Alterei a logo da sidebar quando ela é minimizada

--- _home/static/home/js/home.js
+++ _home/static/home/js/home.js
@@ -3,16 +3,30 @@ document.addEventListener('DOMContentLoaded', function() {
     const toggleButton = document.getElementById("toggle");
     const userToggle = document.getElementById("user-toggle");
     const userDropdown = document.getElementById("user-dropdown");
+    const logoImg = document.getElementById("logo-img");
+    
+    // Função para atualizar o logo
+    function updateLogo() {
+        const isCollapsed = sidebar.classList.contains("collapsed");
+        if (isCollapsed) {
+            logoImg.src = logoImg.dataset.iconLogo;
+        } else {
+            logoImg.src = logoImg.dataset.fullLogo;
+        }
+    }
+
     
     // Restaurar estado da sidebar do localStorage
     const sidebarState = localStorage.getItem('home-sidebar-collapsed');
     if (sidebarState === 'true') {
         sidebar.classList.add("collapsed");
     }
+    updateLogo(); // Atualiza o logo no carregamento da página
     
     // Toggle da sidebar com cache
     toggleButton.addEventListener("click", function () {
         sidebar.classList.toggle("collapsed");
+        updateLogo(); // Atualiza o logo ao clicar no botão
         
         // Salvar estado no localStorage
         const isCollapsed = sidebar.classList.contains("collapsed");
