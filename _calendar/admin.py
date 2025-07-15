c2dbe719489c4a659ebd48d185a1fff088bdcbae
Gabriel Souza <gabriel.souza@sinasc.com.br>
2025-07-15 17:05:23 -0300

    Removi toda a lógica e visual da página de eventos e deixei somente o calendário

--- _calendar/admin.py
+++ _calendar/admin.py
@@ -1,15 +1,1 @@
 from django.contrib import admin
-from _calendar.models import Event
-
-
-@admin.register(Event)
-class EventAdmin(admin.ModelAdmin):
-    list_display = ('title', 'start', 'end', 'color', 'creator', 'created_at')
-    list_filter = ('color', 'all_day', 'creator')
-    search_fields = ('title', 'description')
-    date_hierarchy = 'start'
-
-    def save_model(self, request, obj, form, change):
-        if not obj.creator:
-            obj.creator = request.user
-        super().save_model(request, obj, form, change)
--- _calendar/forms.py
+++ _calendar/forms.py
@@ -1,15 +1,1 @@
 from django import forms
-from _calendar.models import Event
-
-
-class EventForm(forms.ModelForm):
-    """Formulário para criar e editar eventos do calendário"""
-
-    class Meta:
-        model = Event
-        fields = ['title', 'description', 'start', 'end', 'all_day', 'color']
-        widgets = {
-            'start': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
-            'end': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
-            'description': forms.Textarea(attrs={'rows': 3}),
-        }
