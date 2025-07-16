ecf980d9bd245bd50c629b4c842d3fabe15a6ee4
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2025-07-15 23:44:14 -0300

    Adicionei a view get_reunioes e a rota correspondente para obter as reuniões

--- _calendar/urls.py
+++ _calendar/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from _calendar.views import home_view
+from _calendar.views import home_view, get_reunioes
 
 app_name = 'calendar'
 
 urlpatterns = [
     path('', home_view, name='home'),
+    path('api/get/', get_reunioes, name='get_reunioes'),
 ]
--- _calendar/views.py
+++ _calendar/views.py
@@ -1,7 +1,36 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from _calendar.models import Agenda
+from django.http import JsonResponse
 
 
 @login_required
 def home_view(request):
     return render(request, 'calendar/main.html')
+
+
+@login_required
+def get_reunioes(request):
+    reunioes = Agenda.objects.all()
+    
+    if not request.user.has_perm('_calendar.view_reuniao'):
+        reunioes = reunioes.filter(privada=False)
+
+    data = []
+    for reuniao in reunioes:
+        data.append({
+            'id': reuniao.id,
+            'allDay': False,
+            'start': reuniao.data_inicio.isoformat(),
+            'end': reuniao.data_fim.isoformat(),
+            'title': reuniao.titulo,
+            'backgroundColor': reuniao.tipo.cor if reuniao.tipo else None,
+            'borderColor': reuniao.tipo.cor if reuniao.tipo else None,
+            'extendedProps': {
+                'organizer': reuniao.organizador,
+                'description': reuniao.descricao if reuniao.descricao else None,
+                'type': reuniao.tipo.tipo if reuniao.tipo else None,
+                'private': reuniao.privada,
+            }
+        })
+    return JsonResponse(data, safe=False)
