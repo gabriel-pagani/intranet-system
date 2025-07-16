9d7a948fb35fc47659e97360e54d0514da90b3da
Gabriel Souza <gabriel.souza@sinasc.com.br>
2025-07-16 16:14:46 -0300

    Adicionei rotas para adicionar, atualizar e deletar reuniões

--- _calendar/urls.py
+++ _calendar/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from _calendar.views import home_view, get_reunioes
+from .views import home_view, get_reunioes, add_reuniao, update_reuniao, delete_reuniao
 
 app_name = 'calendar'
 
 urlpatterns = [
     path('', home_view, name='home'),
     path('api/get/', get_reunioes, name='get_reunioes'),
+    path('api/add/', add_reuniao, name='add_reuniao'),
+    path('api/update/<int:pk>/', update_reuniao, name='update_reuniao'),
+    path('api/delete/<int:pk>/', delete_reuniao, name='delete_reuniao'),
 ]
