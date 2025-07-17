caa3f3d2fde5bed806ab49423de815f147929265
Gabriel Souza <gabriel.souza@sinasc.com.br>
2025-07-17 08:14:57 -0300

    Criei as views para as rotas da página da agenda

--- _calendar/views.py
+++ _calendar/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from _calendar.models import Agenda
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required, permission_required
+from django.views.decorators.http import require_http_methods
 from django.http import JsonResponse
+from .models import Agenda
 
 
 @login_required
@@ -12,8 +13,8 @@ def home_view(request):
 @login_required
 def get_reunioes(request):
     reunioes = Agenda.objects.all()
-    
-    if not request.user.has_perm('_calendar.view_reuniao'):
+
+    if not request.user.has_perm('_calendar.view_agenda'):
         reunioes = reunioes.filter(privada=False)
 
     data = []
@@ -24,8 +25,8 @@ def get_reunioes(request):
             'start': reuniao.data_inicio.isoformat(),
             'end': reuniao.data_fim.isoformat(),
             'title': reuniao.titulo,
-            'backgroundColor': reuniao.tipo.cor if reuniao.tipo else None,
-            'borderColor': reuniao.tipo.cor if reuniao.tipo else None,
+            'backgroundColor': reuniao.tipo.cor if reuniao.tipo else '#007bff',
+            'borderColor': reuniao.tipo.cor if reuniao.tipo else '#007bff',
             'extendedProps': {
                 'organizer': reuniao.organizador,
                 'description': reuniao.descricao if reuniao.descricao else None,
@@ -34,3 +35,28 @@ def get_reunioes(request):
             }
         })
     return JsonResponse(data, safe=False)
+
+
+@login_required
+@permission_required('_calendar.add_agenda', raise_exception=True)
+def add_reuniao(request):
+    ...
+
+
+@login_required
+@permission_required('_calendar.change_agenda', raise_exception=True)
+def update_reuniao(request, pk):
+    ...
+
+
+@login_required
+@permission_required('_calendar.delete_agenda', raise_exception=True)
+@require_http_methods(["DELETE"])
+def delete_reuniao(request, pk):
+    try:
+        reuniao = get_object_or_404(Agenda, pk=pk)
+        reuniao.delete()
+        return JsonResponse({'status': 'success'})
+        
+    except Exception as e:
+        return JsonResponse({'status': 'error', 'message': str(e)}, status=500)
