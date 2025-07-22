4154d82aaa0c78be05ef6b1d42e9aac1f756fa2b
Gabriel Souza <gabriel.souza@sinasc.com.br>
2025-07-22 10:44:41 -0300

    Criei a rota e a view para usar o novo campo do model de indicadores

--- _dashboards/urls.py
+++ _dashboards/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from _dashboards.views import home_view
+from _dashboards.views import home_view, toggle_favorite
 
 app_name = 'dashboards'
 
 urlpatterns = [
     path('', home_view, name='home'),
+    path('api/toggle-favorite/<int:dashboard_id>/', toggle_favorite, name='toggle_favorite'),
 ]
--- _dashboards/views.py
+++ _dashboards/views.py
@@ -1,19 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from _dashboards.utils.dashboards import metabase, access
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from .models import Dashboard
 
 
 @login_required
 def home_view(request):
-    dados = dict()
+    user = request.user
+    accessible_dashboards = access.get_dashboards(user)
+    
+    favorites = []
+    dashboards_by_sector = {}
 
-    for dashboard in access.get_dashboards(request.user):
+    for dashboard in accessible_dashboards:
+        dashboard_data = {
+            'id': dashboard.id,
+            'titulo': dashboard.titulo,
+            'url': metabase.generate_dashboard_url(dashboard.codigo),
+            'is_favorite': user in dashboard.favoritado_por.all()
+        }
+        
+        if dashboard_data['is_favorite']:
+            favorites.append(dashboard_data)
+        
         setor = dashboard.setor
-        dados.setdefault(setor, []).append(
-            {
-                'titulo': dashboard.titulo,
-                'url': metabase.generate_dashboard_url(dashboard.codigo)
-            }
-        )
+        dashboards_by_sector.setdefault(setor, []).append(dashboard_data)
 
-    return render(request, 'dashboards/main.html', {'dados': dados})
+    return render(request, 'dashboards/main.html', {
+        'favorites': favorites,
+        'dashboards_by_sector': dashboards_by_sector
+    })
+
+
+@login_required
+@require_POST
+def toggle_favorite(request, dashboard_id):
+    dashboard = get_object_or_404(Dashboard, id=dashboard_id)
+    user = request.user
+
+    if user in dashboard.favoritado_por.all():
+        dashboard.favoritado_por.remove(user)
+        is_favorite = False
+    else:
+        dashboard.favoritado_por.add(user)
+        is_favorite = True
+        
+    return JsonResponse({'status': 'success', 'is_favorite': is_favorite})
