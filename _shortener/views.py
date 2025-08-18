2f9d8f56fb72a951fb05f1eb2282c661ebdb87ed
Gabriel Pagani de Souza <gabriel.souza@gruposmi.com.br>
2025-08-18 11:40:07 -0300

    Implementei a lógica da expiração das urls

--- _shortener/views.py
+++ _shortener/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import redirect, get_object_or_404
+from django.http import Http404
+from django.utils import timezone
 from .models import Link
 
 
 def redirect_link(request, slug):
     link = get_object_or_404(Link, slug=slug)
+    
+    if link.expires_at and link.expires_at < timezone.now():
+        raise Http404("Este link expirou")
+    
     link.access_count += 1
     link.save()
     return redirect(link.original_url)
