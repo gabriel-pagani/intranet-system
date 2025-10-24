7737d5ce60f300d7f4c205284c10959958257a0b
Gabriel Pagani de Souza <gabriel.souza@gruposmi.com.br>
2025-10-24 10:45:29 -0300

    Adicionei mais campos para consultar os ramais

--- _ramais/admin.py
+++ _ramais/admin.py
@@ -4,6 +4,6 @@ from _ramais.models import Ramais
 
 @admin.register(Ramais)
 class RamaisAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'ramal', 'setor', 'maquina')
-    search_fields = ('nome', 'setor')
+    list_display = ('nome', 'ramal', 'setor', 'maquina',)
+    search_fields = ('nome', 'ramal', 'setor', 'maquina',)
     list_filter = ('setor',)
